cbfb1dda001648798039bdf8d2515b5ea71b3051
JaniduSathsara <janidusathsara497@gmail.com>
2026-09-23 11:24:14 +0530

    Revert "delete example java"
    This reverts commit 0f73149f39a257376176ad462b56b2aa18abb9c6.

---
+++ Example.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("hey");
+
+        int num = 10;
+// commit karamu
+        for (int i = 0; i < num; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
