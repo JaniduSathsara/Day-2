b8b4a7513b938442c5b74c0f987dfe48b8dc1236
JaniduSathsara <janidusathsara497@gmail.com>
2026-09-23 11:12:51 +0530

    Add example file

---
+++ Example.java
@@ -0,0 +1,16 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("hey");
+
+        int num = 10;
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("commit 01");
+
+        System.out.println("commit 01");
+    }
+}
